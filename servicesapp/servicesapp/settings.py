ec3dada81df4b07a4ee3b0a4b6f0b7abe074cf79
steve <mainastephen159@gmail.com>
2026-06-21 18:44:49 +0300

    updates

--- servicesapp/servicesapp/settings.py
+++ servicesapp/servicesapp/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)*yd81kbl9#2f9xa#=-k^=rz!fvjejup2y3mkw+)szbr)1ag$3'
+SECRET_KEY = os.environ("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -195,6 +195,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mainastephen159@gmail.com'
-EMAIL_HOST_PASSWORD = 'geztfbutyghmbiua' 
-DEFAULT_FROM_EMAIL = 'BlueConnect <mainastephen159@gmail.com>'
+EMAIL_HOST_USER = os.environ("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.environ("DEFAULT_FROM_EMAIL")
